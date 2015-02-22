ba0b7a8f188b346df0115a7dce27a6e17ee29f7d
Dylan <vuzdylan@gmail.com>
2015-02-22 13:57:29 -0500

    edited to new json fromat

--- chrome-vr/lib/client-net.js
+++
@@ -1,9 +0,0 @@
-//used to parse our json objects
-function parse( jsonObject){
-    console.log("Parse");
-    console.log(jsonObject);
-    var obj = JSON.parse(jsonObject);
-    console.log(obj);
-    var vector = new THREE.Vector3(obj.X,obj.Y,obj.Z);
-    return vector;
-}
--- chrome-vr/lib/ioClient.js
+++ chrome-vr/lib/ioClient.js
@@ -9,26 +9,53 @@ socket.on('connect', function () {
 socket.on('disconnect', function(){
 	consolelog("opps client disconnect");
 });
-var lastNode = null;
+
+var LHandDraw = null;
+var RHandDraw = null;
 
 //handle a drawing event
-socket.on('draw', function(data) {
+socket.on('data', function(data) {
 	console.log(data);
-	if (lastNode === null | lastNode === {}){
-		lastNode = parse(data);
-	}else{
-		var newNode = parse(data);
-		drawLine(lastNode,newNode);
-		lastNode = newNode;
+	//hopfully the json is formed correctly...
+	try{
+		//Move us if we have moved
+		var obj = JSON.parse(data);
+		var move = obj.data.torso;
+		controls.move(move);
+
+		//check to see if the left hand should be drawing
+		//if not set LHand to null
+		if(data.LHand.active){
+			//if LHandDraw is null set it
+			//else draw a line with the new data and LHandDraw then swap
+			if (LHandDraw === null){
+				LHandDraw = new THREE.Vector3(data.LHand.x,data.LHand.y,data.LHand.z);
+			}else{
+				var leftTemp = new THREE.Vector3(data.LHand.x,data.LHand.y,data.LHand.z);
+				drawLine(LHandDraw,leftTemp);
+				LHandDraw = leftTemp;
+			}
+		}else{
+			LHandDraw = null;
+		}
+		//same as above just all right hand
+		if(data.RHand.active){
+			if (RHandDraw === null){
+				RHandDraw = new THREE.Vector3(data.RHand.x,data.RHand.y,data.RHand.z);
+			}else{
+				var rightTemp = new THREE.Vector3(data.RHand.x,data.RHand.y,data.RHand.z);
+				drawLine(RHandDraw,rightTemp);
+				RHandDraw = rightTemp;
+			}
+		}else{
+			RHandDraw = null;
+		}
+	}catch(err){
+		console.log(err);
 	}
 });
 
-//handle a movment event
-socket.on('move',function(data){
-	console.log(data);
-	var move = parse(data);
-	controls.move(move);
-});
+
 
 socket.on('disconnect', function() {
 	console.log('client disconnected');
